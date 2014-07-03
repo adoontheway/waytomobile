621bbb1c08dfc3332e93d87f3c8984668fd6775c
Ado <jerry.l.du@hotmail.com>
2014-07-03 18:15:15 +0800

    dragonbones切换状态

--- projects/game003/scripts/app/scenes/AnotherScene.lua
+++ projects/game003/scripts/app/scenes/AnotherScene.lua
@@ -3,13 +3,21 @@ local AnotherScene = class("AnotherScene", function()
 end)
 
 function AnotherScene:ctor()
+	self.curBehaviorId = 1;
     self.layer = display.newLayer()
     self:addChild(self.layer)
     self.layer:setTouchEnabled(true)
+    self.behaviors = {"anim_walk","anim_eat","anim_placeladder","anim_idle","anim_ladderwalk","anim_laddereat","anim_death"};
 end
 
 function AnotherScene:onTouch(event, x, y)
-
+	print("Touched at %d %d",x,y)
+	self.curBehaviorId = self.curBehaviorId + 1;
+	if self.curBehaviorId > #self.behaviors then
+		self.curBehaviorId = 1;
+	end
+	print("Now playing ", self.curBehaviorId , #self.behaviors, self.behaviors[self.curBehaviorId])
+	self.animation:play(self.behaviors[self.curBehaviorId])
 end
 
 function AnotherScene:onEnter()
@@ -25,9 +33,9 @@ function AnotherScene:onEnter()
     zombie:connectMovementEventSignal(function(__evtType, __moveId)
 			echoInfo("movement, evtType: %d, moveId: %s", __evtType, __moveId)
 		end)
-    local animation = zombie:getAnimation()
-    animation:setAnimationScale(0.5)
-    animation:play("anim_walk")
+    self.animation = zombie:getAnimation()
+    self.animation:setAnimationScale(0.5)
+    self.animation:play("anim_walk")
     zombie:setPosition(display.cx, display.cy)
     self.layer:addChild(zombie)
     self.layer:addTouchEventListener(function(event,x,y)
