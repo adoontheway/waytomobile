fbf119d3048abfb8c5f571a6be8109f18c64907d
Ado <jerry.l.du@hotmail.com>
2014-07-04 14:16:10 +0800

    the walking dead.....

--- projects/game003/scripts/app/scenes/AnotherScene.lua
+++ projects/game003/scripts/app/scenes/AnotherScene.lua
@@ -3,11 +3,16 @@ local AnotherScene = class("AnotherScene", function()
 end)
 
 function AnotherScene:ctor()
+	self.speed = 2;
 	self.curBehaviorId = 1;
     self.layer = display.newLayer()
     self:addChild(self.layer)
     self.layer:setTouchEnabled(true)
     self.behaviors = {"anim_walk","anim_eat","anim_placeladder","anim_idle","anim_ladderwalk","anim_laddereat","anim_death"};
+    self.bg = display.newSprite("battle.png",display.cx, display.cy)
+    self.bg1 = display.newSprite("battle.png",display.cx+self.bg:getContentSize().width, display.cy)
+    self.layer:addChild(self.bg)
+    self.layer:addChild(self.bg1)
 end
 
 function AnotherScene:onTouch(event, x, y)
@@ -20,10 +25,28 @@ function AnotherScene:onTouch(event, x, y)
 	self.animation:play(self.behaviors[self.curBehaviorId])
 end
 
+function AnotherScene:onEnterFrame(dt)
+	local tempX = self.bg:getPositionX();
+	local tempW = self.bg:getContentSize().width;
+	tempX = tempX - self.speed;
+	if tempX <= display.cx - tempW then
+		self.bg:setPositionX(display.cx + tempW)
+	else
+		self.bg:setPositionX(tempX)
+	end
+
+	tempX = self.bg1:getPositionX();
+	tempW = self.bg1:getContentSize().width;
+	tempX = tempX - self.speed;
+	if tempX <= display.cx - tempW then
+		self.bg1:setPositionX(display.cx + tempW)
+	else
+		self.bg1:setPositionX(tempX)
+	end
+end
+
 function AnotherScene:onEnter()
-    self.bg = display.newSprite("battle.png",display.cx, display.cy)
-    self.layer:addChild(self.bg)
-    ui.newTTFLabel({text = "AnotherScene", size = 64, align = ui.TEXT_ALIGN_CENTER})
+    ui.newTTFLabel({text = "Yo! Yo! Check Out!", size = 64, align = ui.TEXT_ALIGN_CENTER})
         :pos(display.cx, display.cy)
         :addTo(self.layer)
 
@@ -37,10 +60,14 @@ function AnotherScene:onEnter()
     self.animation:setAnimationScale(0.5)
     self.animation:play("anim_walk")
     zombie:setPosition(display.cx, display.cy)
+    zombie:setScaleX(-1)
     self.layer:addChild(zombie)
     self.layer:addTouchEventListener(function(event,x,y)
     	return self:onTouch(event, x,y)
     	end)	
     self.layer:setTouchEnabled(true)
+    self:scheduleUpdate(function(dt) 
+    	self:onEnterFrame(dt)
+    end)
 end
 return AnotherScene
